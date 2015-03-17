47aae62c153f16974188ba6e2eefedb70d470718
diomedes <diomedex10@gmail.com>
2015-03-16 23:51:40 -0500

    crud

--- model/Conection.php
+++ model/Conection.php
@@ -5,6 +5,9 @@ namespace model;
 abstract class Conection
 {
     protected $_conection;
+    private $_host;
+    private $_password;
+    private $_usuario;
 
     public function __construct()
     {
