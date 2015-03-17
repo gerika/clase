da3af49d74c8f1e2c292609df4a2a11e33a19b46
giuliana <giuliana27_10@hotmail.com>
2015-03-16 22:40:06 -0500

    cambioo

--- Calcular.php
+++ Calcular.php
@@ -1,11 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Aureliano III
- * Date: 15/03/2015
- * Time: 05:56 PM
- */
+include'Triangulo.php';
+include'Cuadrado.php';
+include 'Adapter.php';
 
-class Calcular extends Adapter{
+class Calcular  implements Adapter {
+      private $conexion;
+    public function setConexion($tipo_figura)
+    {
+        if($tipo_figura=="Triangulo")
+        {
+            (object)$this->conexion=new Triangulo(3,3);
+           // $this->conexion="triangulo";
+        }
+        else
+        {
+            if($tipo_figura=="Cuadrado")
+            {
+                (object)$this->conexion=new Cuadrado(3,4);
 
+            }
+        }
+    }
+    public function getConexion()
+    {
+        return (object)$this->conexion;
+    }
 }
+$c=new Calcular();
+$c->setConexion("Triangulo");
+
+(object)$n= $c->getConexion();
+echo get_class($n);
+$n->calcular_area(3);
+
+
+
+
+
+
