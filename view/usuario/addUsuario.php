b54ce8a724cf4c8f457a5d8c2d5b8deeacd499ba
diomedes <diomedex10@gmail.com>
2015-03-20 22:24:55 -0500

    .

--- view/usuario/addUsuario.php
+++ view/usuario/addUsuario.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: diomedes
- * Date: 16/03/15
- * Time: 10:33 PM
- */
+
--- view/usuario/editUsuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: diomedes
- * Date: 16/03/15
- * Time: 10:33 PM
- */
